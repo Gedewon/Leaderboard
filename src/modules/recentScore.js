14423e8f25980832863dea037107b91dd415a6b7
Gedewon <gedewon.hayle.9@gmail.com>
2022-05-13 00:31:29 +0300

    Replace dume data

--- src/modules/recentScore.js
+++ src/modules/recentScore.js
@@ -1,30 +1,37 @@
+import ApiClient from './getScore.js';
 export default () => {
-  const wrapper = document.createElement('div');
-  wrapper.classList.add('scores');
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('scores');
 
-  const heading = document.createElement('div');
-  heading.classList.add('heading');
+    const heading = document.createElement('div');
+    heading.classList.add('heading');
 
-  const headingText = document.createElement('h3');
-  headingText.innerText = 'Recent scores';
-  heading.appendChild(headingText);
+    const headingText = document.createElement('h3');
+    headingText.innerText = 'Recent scores';
+    heading.appendChild(headingText);
 
-  const refreshBtn = document.createElement('button');
-  refreshBtn.type = 'button';
-  refreshBtn.classList.add('refresh-btn');
-  refreshBtn.innerText = 'Refresh';
-  heading.appendChild(refreshBtn);
+    const refreshBtn = document.createElement('button');
+    refreshBtn.type = 'button';
+    refreshBtn.classList.add('refresh-btn');
+    refreshBtn.innerText = 'Refresh';
+    heading.appendChild(refreshBtn);
+    refreshBtn.onclick = () => {
+        window.location.reload();
+    };
 
-  const scoreList = document.createElement('ul');
-  scoreList.classList.add('scores-list');
-  scoreList.innerHTML = `<li class="recent-score"> Name: 100</li>
-                          <li class="recent-score"> Name: 20</li>
-                          <li class="recent-score"> Name: 50</li>
-                          <li class="recent-score"> Name: 78</li>
-                          <li class="recent-score"> Name: 125</li>
-                          <li class="recent-score"> Name: 77</li>
-                          <li class="recent-score"> Name: 42</li>`;
-  wrapper.appendChild(heading);
-  wrapper.appendChild(scoreList);
-  return wrapper;
+    const scoreList = document.createElement('ul');
+    scoreList.classList.add('scores-list');
+    ApiClient.getScores().then(
+        (scores) => {
+            scoreList.innerHTML = '';
+            scores.result.forEach((score) => {
+                scoreList.innerHTML += `<li class="recent-score"> ${score.user}: ${score.score}</li>`;
+            });
+        },
+    ).catch(() => {
+        scoreList.innerHTML = ' Error Loading ';
+    });
+    wrapper.appendChild(heading);
+    wrapper.appendChild(scoreList);
+    return wrapper;
 };
